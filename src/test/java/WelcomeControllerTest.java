aa2311808170ae6a51af843d02dd014079744d17
Sokkalingam Subramanian <sokkalingam.subramanian@amadeus.com>
2018-04-12 22:44:49 -0400

    Welcome Controller Test

---
+++ src/test/java/WelcomeControllerTest.java
@@ -0,0 +1,23 @@
+import instagram.WebApp;
+import org.hamcrest.Matchers;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = WebApp.class)
+public class WelcomeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @org.junit.Test
+    public void welcomeTest() {
+        assertThat(this.restTemplate.getForObject("/", String.class), equalTo("Welcome!"));
+    }
+}
