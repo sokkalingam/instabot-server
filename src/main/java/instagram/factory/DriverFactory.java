182714dd45b59d25e3cfbe4b7c0f8543d7f3c662
Sokkalingam Subramanian <sokkalingam.subramanian@amadeus.com>
2018-04-14 13:00:51 -0400

    ChromeDriver service

--- src/main/java/instagram/factory/DriverFactory.java
+++ src/main/java/instagram/factory/DriverFactory.java
@@ -1,19 +1,28 @@
 package instagram.factory;
 
+import com.google.common.collect.ImmutableMap;
 import instagram.model.Data;
 import instagram.model.ConfigData;
 import instagram.utils.NavigationUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
 
 public class DriverFactory {
 
     public static WebDriver getLoggedInDriver(Data data) {
-        System.setProperty(ConfigData.DRIVER_PROPERTY_NAME, ConfigData.DRIVER_PROPERTY_VALUE);
+//        System.setProperty(ConfigData.DRIVER_PROPERTY_NAME, ConfigData.DRIVER_PROPERTY_VALUE);
+        ChromeDriverService chromeDriverService = new ChromeDriverService.Builder()
+                        .usingDriverExecutable(new File(ConfigData.DRIVER_PROPERTY_VALUE))
+                        .usingAnyFreePort()
+                        .withEnvironment(ImmutableMap.of("DISPLAY",":20"))
+                        .build();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
         NavigationUtils.setup(driver, data.sessionId);
         return driver;
     }
