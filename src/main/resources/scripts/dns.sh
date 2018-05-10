8c2346d148dae770df91c0c566a5733d33db3ca8
Sokkalingam Subramanian <lings24@gmail.com>
2018-05-10 06:50:52 -0400

    Improved DNS shell script

--- src/main/resources/scripts/dns.sh
+++ src/main/resources/scripts/dns.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+URL="$1.localtunnel.me"
+
 while true
-    do
-        echo "Building DNS for $1:$2"
-        lt --subdomain $1 --port $2
-        sleep 5
-    done
+do
+	echo "Building DNS for $URL:$2"
+	
+	# Run Local Tunnel for subdomain param1 and port param2,
+	# write the output to ltOut.log
+	# Run it as background process using & at the end
+	lt --subdomain $1 --port $2 > ltOut.log &
+	
+	# Store the process id of lt background process in pid
+	pid=$!
+	sleep 10
+	
+	# Read ltOut.log and store it to variable output
+	output=$(<ltOut.log)
+	echo "$output"
+	
+	# if output contains the desired URL
+	if [[ $output = *$URL ]]
+	then
+		# wait till the lt process completes
+		echo "Running on $pid"
+		wait $pid
+	else
+		# kill all node.exe processes if the URL does not match what we want
+		taskkill -F -IM node.exe
+	fi
+	sleep 10
+
+done
