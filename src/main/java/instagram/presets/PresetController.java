ab596c846b98d92bd0b3965e98ff5d5b313d7adf
subramanians <sokkalingam.subramanian@amadeus.com>
2018-09-10 16:44:32 -0400

    Cookie for Preset to lookup preset on load

--- src/main/java/instagram/presets/PresetController.java
+++ src/main/java/instagram/presets/PresetController.java
@@ -2,22 +2,34 @@ package instagram.presets;
 
 import instagram.model.PresetData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;q
 
 @RestController
-@RequestMapping("/api/presets/")
+@RequestMapping("/api/presets")
 public class PresetController {
 
     @Autowired
     private PresetService presetService;
 
+    private static final String PRESET_NAME = "presetName";
+
+    @RequestMapping("/")
+    public PresetData getPresetByCookie(@CookieValue(value = PRESET_NAME, defaultValue = "") String presetName,
+                                HttpServletResponse response) {
+        return getPresetByName(presetName, response);
+    }
+
     @RequestMapping("/{presetName}")
-    public PresetData getPreset(@PathVariable String presetName) {
+    public PresetData getPresetByName(@PathVariable String presetName, HttpServletResponse response) {
+
+        if (!StringUtils.isEmpty(presetName))
+            response.addCookie(new Cookie(PRESET_NAME, presetName));
+
         return presetService.getPreset(presetName);
     }
 
