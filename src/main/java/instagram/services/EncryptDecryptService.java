3f7f815519260a38645ac3c272d07c3663b09fd7
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2018-10-05 16:44:39 -0400

    Encrypt and Decrypt

---
+++ src/main/java/instagram/services/EncryptDecryptService.java
@@ -0,0 +1,69 @@
+package instagram.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * Program to Encrypt/Decrypt String Using AES 128 bit Encryption Algorithm
+ */
+@Service
+public class EncryptDecryptService {
+
+    @Value("${key:word}")
+    private String encryptionKey;
+    private final String characterEncoding       = "UTF-8";
+    private final String cipherTransformation    = "AES/CBC/PKCS5PADDING";
+    private final String aesEncryptionAlgorithem = "AES";
+
+    /**
+     * Method for Encrypt Plain String Data
+     * @param plainText
+     * @return encryptedText
+     */
+    public String encrypt(String plainText) {
+        String encryptedText = "";
+        try {
+            Cipher cipher   = Cipher.getInstance(cipherTransformation);
+            byte[] key      = encryptionKey.getBytes(characterEncoding);
+            SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
+            IvParameterSpec ivparameterspec = new IvParameterSpec(key);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivparameterspec);
+            byte[] cipherText = cipher.doFinal(plainText.getBytes("UTF8"));
+            Base64.Encoder encoder = Base64.getEncoder();
+            encryptedText = encoder.encodeToString(cipherText);
+
+        } catch (Exception E) {
+            System.err.println("Encrypt Exception : "+E.getMessage());
+        }
+        return encryptedText;
+    }
+
+    /**
+     * Method For Get encryptedText and Decrypted provided String
+     * @param encryptedText
+     * @return decryptedText
+     */
+    public String decrypt(String encryptedText) {
+        String decryptedText = "";
+        try {
+            Cipher cipher = Cipher.getInstance(cipherTransformation);
+            byte[] key = encryptionKey.getBytes(characterEncoding);
+            SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
+            IvParameterSpec ivparameterspec = new IvParameterSpec(key);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivparameterspec);
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] cipherText = decoder.decode(encryptedText.getBytes("UTF8"));
+            decryptedText = new String(cipher.doFinal(cipherText), "UTF-8");
+
+        } catch (Exception E) {
+            System.err.println("decrypt Exception : "+E.getMessage());
+        }
+        return decryptedText;
+    }
+
+}
