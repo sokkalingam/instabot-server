6c41b803ca0ac46b49789ef2582e229cac4ee633
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2018-11-12 12:13:20 -0500

    HTML emails for reports

---
+++ src/main/java/instagram/email/EmailHelper.java
@@ -0,0 +1,51 @@
+package instagram.email;
+
+import instagram.model.Report;
+import instagram.utils.DateUtils;
+
+public class EmailHelper {
+
+    public static String getHtmlReport(Report report) {
+        return "<h1><span style=\"color: #800080;\">InstaBot Report</span></h1>\n" +
+                "<table style=\"height: 228px; width: 516px;\">\n" +
+                "<tbody>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">Job Status</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + report.getJobStatus() + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">Start Time</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + DateUtils.format(report.getStartTime()) + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">End Time</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + DateUtils.format(report.getEndTime()) + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">Loop Count</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + report.getCurrentLoop() + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">Photos Liked</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + report.getPhotosLiked() + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 178px;\"><strong><span style=\"color: #800080;\">Photos Commented</span></strong></td>\n" +
+                "<td style=\"width: 342px;\">\n" +
+                "<pre><strong>" + report.getPhotosCommented() + "</strong></pre>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "</tbody>\n" +
+                "</table>";
+    }
+}
--- src/main/java/instagram/email/EmailService.java
+++ src/main/java/instagram/email/EmailService.java
@@ -3,6 +3,7 @@ package instagram.email;
 import instagram.messages.EmailMessages;
 import instagram.model.Data;
 import instagram.model.Report;
+import instagram.model.enums.JobStatus;
 import instagram.report.ReportService;
 import instagram.services.EncryptDecryptService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Service
 public class EmailService {
@@ -31,12 +38,35 @@ public class EmailService {
     private EncryptDecryptService encryptDecryptService;
 
     public void testEmail(String to) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("Instabot");
-        message.setTo(to);
-        message.setSubject("Hello from Instabot");
-        message.setText("This is a test email from Instabot");
-        sendEmail(message);
+        try {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+            Report report = new Report("username");
+            report.setJobStatus(JobStatus.COMPLETED);
+            report.setStartTime(LocalDateTime.now());
+            report.setEndTimeAsNow();
+            report.setCurrentLoop(24);
+            mimeMessage.setContent(EmailHelper.getHtmlReport(report), "text/html");
+            mimeMessageHelper.setTo("lings24@gmail.com");
+            mimeMessageHelper.setSubject("Test Email for HTML from Instabot");
+            sendEmail(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean sendEmail(MimeMessage message) {
+        emailSender.setUsername(encryptDecryptService.decrypt(email));
+        emailSender.setPassword(encryptDecryptService.decrypt(password));
+        try {
+            emailSender.send(message);
+            System.out.println("Email Sent to: " + Arrays.asList(message.getAllRecipients()));
+            return true;
+        } catch (MailException | MessagingException e) {
+            System.out.println("Could not send email, SimpleMailMessage: " + message);
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean sendEmail(SimpleMailMessage message) {
@@ -53,30 +83,45 @@ public class EmailService {
     }
 
     public void sendJobFinishedEmail(Data data) {
-        SimpleMailMessage message = getBasicEmail(data);
-        message.setSubject(EmailMessages.JOB_FINISHED.toString());
-        sendEmail(message);
+        try {
+            MimeMessage message = getBasicEmail(data);
+            message.setSubject(EmailMessages.JOB_FINISHED.toString());
+            sendEmail(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendJobAbortedEmail(Data data) {
-        SimpleMailMessage message = getBasicEmail(data);
-        message.setSubject(EmailMessages.JOB_ABORTED.toString());
-        sendEmail(message);
+        try {
+            MimeMessage message = getBasicEmail(data);
+            message.setSubject(EmailMessages.JOB_ABORTED.toString());
+            sendEmail(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void sendJobAbortedForMaintenanceEmail(Data data) {
-        SimpleMailMessage message = getBasicEmail(data);
-        message.setSubject(EmailMessages.JOB_ABORTED_FOR_MAINTENACE.toString());
-        sendEmail(message);
+        try {
+            MimeMessage message = getBasicEmail(data);
+            message.setSubject(EmailMessages.JOB_ABORTED_FOR_MAINTENACE.toString());
+            sendEmail(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
-    public SimpleMailMessage getBasicEmail(Data data) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(getToList(data.email));
+    public MimeMessage getBasicEmail(Data data) throws MessagingException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
         Report report = reportService.getReport(data.username);
-        if (report != null)
-            message.setText(String.valueOf(report));
-        return message;
+        if (report != null) {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+            mimeMessageHelper.setTo(getToList(data.email));
+            mimeMessage.setContent(EmailHelper.getHtmlReport(report), "text/html");
+        }
+        return mimeMessage;
     }
 
     public String getEmail() {
